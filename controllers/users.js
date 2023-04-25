ca231bc1751065f7d2e3a4ab8d97ecf75ccbfb65
Edu <eduardo.sbassani@gmail.com>
2023-04-25 11:44:07 +0300

    Hashing passwords for users

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,21 @@
 const User = require('../models/users');
 
+const bcrypt = require('bcrypt');
+
 exports.createUser = async (req, res) => {
     const { username, email, password } = req.body;
-  
+
+    const salt = await bcrypt.genSalt(10);
+    const pass = await bcrypt.hash(password, salt);
+
+    /* Check if password is the same as hash 
+    const result = await bcrypt.compare(plaintextPassword, hash);
+    return result; */
+    
     const user = new User({ 
-        username, 
+        username,
         email, 
-        password 
+        password: pass
     });
   
     await user.save();
