0ce4fac1569710832a787edaa2a266471dc5acb9
Edu <eduardo.sbassani@gmail.com>
2023-04-27 14:07:28 +0300

    Full card API and update Card model

--- models/card.js
+++ models/card.js
@@ -7,6 +7,8 @@ const productSchema = new Schema({
 	description: { type: String, required: true },
 	date: { type: Date, default: Date.now },
 	likes: { type: Number, default: 0 },
+	user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    topic_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Topic', required: true }
 })
 
 //mongoose.model create a collection lowercase + add 's' ex:Card => cards
---
+++ routes/cardsAPI.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const controller = require('../controllers/cardsAPI');
+
+const router = express.Router();
+
+router.post('/', controller.createCard);
+
+router.get('/' , controller.getCards);
+
+router.get('/:id', controller.getCard);
+
+router.put('/:id', controller.updateCard);
+
+router.patch('/:id', controller.patchCard);
+
+router.delete('/:id', controller.deleteCard);
+
+router.get('/t/:id' , controller.getTopicCards);
+
+router.get('/u/:id' , controller.getUserCards);
+
+module.exports = router;
