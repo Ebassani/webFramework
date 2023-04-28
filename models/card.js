5b988fe5dedf172ae27c1bb36f7859cb9fbd8d25
Sandu Palii <palii.sandu@gmail.com>
2023-04-28 15:25:39 +0300

    Feature - Adding timestaps for card

--- models/card.js
+++ models/card.js
@@ -2,12 +2,14 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
-const productSchema = new Schema({
-	title: { type: String, required: true },
-	description: { type: String, required: true },
-	date: { type: Date, default: Date.now },
-	likes: { type: Number, default: 0 },
-})
+const productSchema = new Schema(
+	{
+		title: { type: String, required: true },
+		description: { type: String, required: true },
+		likes: { type: Number, default: 0 },
+	},
+	{ timestamps: true }
+)
 
 //mongoose.model create a collection lowercase + add 's' ex:Card => cards
 module.exports = mongoose.model('Card', productSchema)
